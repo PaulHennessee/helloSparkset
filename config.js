b5c4032bf91417f8d2452c8e8c17b9ff3dd76d37
Delia Gomez <dagomez713@gmail.com>
2021-04-27 21:12:35 -0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
         auth: {
         clientId: 'bbe68f28-08fb-4b1c-8043-c1dc402550de', //changed to sparkset
         redirectUri: 'http://localhost:8080',               //remember to change this on live site
-        postLogoutRedirectUri: 'https://admin.hellosparkset.com/settings'
+        postLogoutRedirectUri: 'https://admin.hellosparkset.com/settings' //fix redirect
         }
     },
     msalRequest: {
